081a0e75faa0848774c74bd0b13a66fb437834b6
Samson Lancaster <myrddincrow@gmail.com>
2019-07-02 13:44:02 -0700

    added check all button and items remaining

--- todo-project/src/TodoList.vue
+++ todo-project/src/TodoList.vue
@@ -3,13 +3,20 @@
   <input type="text" class="todo-input" placeholder="what needs to be done" v-model="newTodo" @keyup.enter="addTodo">
  <div v-for="(todo, index) in todos" :key="todo.id" class="todo-item">
    <div class="todo-item-left">
-     <div v-if="!todo.editing" @dblclick="editTodo(todo)" class="todo-item-label">{{ todo.title }}</div>
-     <input v-else class="todo-item-edit" type="text" v-model="todo.title" @blur="doneEdit(todo)" @keyup.enter="doneEdit(todo)">
+     <input type="checkbox" v-model="todo.completed">
+     <div v-if="!todo.editing" @dblclick="editTodo(todo)" class="todo-item-label" :class="{ completed : todo.completed }">{{ todo.title }}</div>
+     <input v-else class="todo-item-edit" type="text" v-model="todo.title" @blur="doneEdit(todo)" @keyup.enter="doneEdit(todo)" @keyup.esc="cancelEdit(todo)" v-focus>
    </div>
 <div class="remove-item" @click="removeTodo(index)">
 &times;
 </div>
  </div>
+
+    <div class="extra-container">
+      <div><label><input type="checkbox" :checked="!anyRemaining" @change="checkAllTodos"> Check All</label></div>
+      <div>{{ remaining }} items left</div>
+
+    </div>
 
   </div>
 </template>
@@ -21,6 +28,7 @@
           return{
             newTodo:'',
             idForTodo: 3,
+            beforeEditCache:'',
             todos: [
               {
                 'id':1,
@@ -37,6 +45,24 @@
             ]
           }
       },
+      computed:{
+        remaining(){
+          return this.todos.filter(todo => !todo.completed).length
+        },
+        anyRemaining(){
+          return this.remaining != 0
+        }
+      },
+
+      directives:{
+          focus:{
+            // directive definition
+            inserted: function (el) {
+              el.focus()
+            }
+          }
+      },
+
       methods: {
           addTodo() {
 
@@ -54,16 +80,31 @@
           },
 
         editTodo(todo){
+            this.beforeEditCache = todo.title
             todo.editing = true
         },
 
         doneEdit(todo){
+          if (todo.title.trim() == ''){
+            todo.title = this.beforeEditCache
+          }
             todo.editing = false
+        },
+
+        cancelEdit(todo){
+          todo.title = this.beforeEditCache
+          todo.editing = false
+
         },
 
         removeTodo(index){
             this.todos.splice(index,1)
+        },
+
+        checkAllTodos(){
+            this.todos.forEach((todo) => todo.completed = event.target.checked)
         }
+
       }
     }
 </script>
@@ -114,9 +155,35 @@
       border: 1px solid #ccc; //override defaults
       font-family: 'Avenir', Helvetica, Arial, sans-serif;
       &:focus{
-        outline: 0;
+        outline: none;
       }
     }
+  .completed {
+    text-decoration: line-through;
+    color:grey;
+  }
 
+  .extra-container {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 16px;
+    border-top:1px solid lightgrey;
+    padding-top: 14px;
+    margin-bottom: 14px;
+  }
+
+  .button {
+    font-size: 14px;
+    background-color: white;
+    appearance:none;
+    &:hover {
+      background: lightgreen;
+    }
+  }
+
+  .active {
+    background: lightgreen;
+  }
 
 </style>
