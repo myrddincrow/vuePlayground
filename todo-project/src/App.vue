ea5ea577fcce83edf02b593aac2b6c05b2e79999
Samson Lancaster <myrddincrow@gmail.com>
2019-07-02 11:55:00 -0700

    added initial basic functionality

--- todo-project/src/App.vue
+++ todo-project/src/App.vue
@@ -1,27 +1,18 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div id="app" class="container">
+    <img class="logo" src="./assets/logo.png">
+    <todo-list></todo-list>
+    <!--<h1>{{ msg }}</h1>-->
+
   </div>
 </template>
 
 <script>
+
+import TodoList from "./TodoList";
 export default {
   name: 'app',
+  components: {TodoList},
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
@@ -31,14 +22,29 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
+
+  *{
+  box-sizing: border-box;
+  }
+  .container {
+    max-width:600px;
+    margin: 0 auto;
+  }
+  #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /*text-align: center;*/
   color: #2c3e50;
   margin-top: 60px;
+    font-size: 24px;
 }
+
+  .logo {
+    display: block;
+    margin: 20px auto;
+    height: 75px;
+  }
 
 h1, h2 {
   font-weight: normal;
---
+++ todo-project/src/TodoList.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+  <input type="text" class="todo-input" placeholder="what needs to be done" v-model="newTodo" @keyup.enter="addTodo"/>
+ <div v-for="(todo, index) in todos" :key="todo.id" class="todo-item">
+   <div>
+     {{ todo.title }}
+   </div>
+<div class="remove-item" @click="removeTodo(index)">
+&times;
+</div>
+ </div>
+
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "TodoList",
+      data (){
+          return{
+            newTodo:'',
+            idForTodo: 3,
+            todos: [
+              {
+                'id':1,
+                'title':'Finish Vue Todo App',
+                'completed':false,
+              },
+              {
+                'id':2,
+                'title':'Do stuff to this',
+                'completed':false,
+              },
+            ]
+          }
+      },
+      methods: {
+          addTodo() {
+
+            if (this.newTodo.trim().length == 0){
+              return
+            }
+
+          this.todos.push({
+            id: this.idForTodo,
+            title: this.newTodo,
+            completed: false,
+          })
+            this.newTodo = ''
+            this.idForTodo++
+          },
+        removeTodo(index){
+            this.todos.splice(index,1)
+        }
+      }
+    }
+</script>
+
+<style lang="scss">
+ .todo-input{
+   width: 100%;
+   padding: 10px 18px;
+   font-size: 18px;
+   margin-bottom: 16px;
+
+   &:focus{
+     outline: 0;
+   }
+ }
+
+  .todo-item {
+    margin-bottom: 12px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .remove-item {
+    cursor: pointer;
+    margin-left: 14px;
+    &:hover{
+      color:red;
+    }
+  }
+</style>
